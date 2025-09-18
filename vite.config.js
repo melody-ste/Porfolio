fdf0bb4f288e202c7456f365dea1a28009ed62c0
melody-ste <melody_stephan@icloud.com>
2025-09-18 15:52:27 +0200

    config for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
+  base: './',
+  server: {
     watch: {
       usePolling: true, // ← active le polling pour /mnt/* sous WSL
       interval: 100,    // optionnel : vérifie toutes les 100ms
